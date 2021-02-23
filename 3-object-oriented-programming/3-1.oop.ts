147f9adb20fc7681c48b74a099a4b2d6e9b7afac
Joy Lee <joylee.dev@gmail.com>
2021-02-23 17:32:18 +0900

    3 - Object Oriented Programming : class

--- 3-object-oriented-programming/3-1.oop.ts
+++ 3-object-oriented-programming/3-1.oop.ts
@@ -4,21 +4,37 @@
     hasMilk: boolean;
   };
 
-  const BEANS_GRAM_PER_SHOT: number = 7;
-  let coffeeBeans: number = 0;
+  // 관련 속성과 함수를 묶어 어떤 형태의 데이터를 만들지 정의하는 것
+  class CoffeeMachine {
+    // member variables
+    static BEANS_GRAM_PER_SHOT: number = 7; // class level : <클래스명.변수명>
+    coffeeBeans: number = 0; // instance(object) level : <this.변수명>
 
-  function makeCoffee(shots: number): CoffeeCup {
-    if (coffeeBeans < shots * BEANS_GRAM_PER_SHOT) {
-      throw new Error('Not enough coffee beans!');
+    // 인스턴스 생성 시 항상 호출되는 함수
+    constructor(coffeeBeans) {
+      this.coffeeBeans = coffeeBeans;
     }
-    coffeeBeans -= shots * BEANS_GRAM_PER_SHOT;
-    return {
-      shots,
-      hasMilk: false,
-    };
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      let requiredBeans = shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+      if (this.coffeeBeans < requiredBeans) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= requiredBeans;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
   }
 
-  coffeeBeans += 3 * BEANS_GRAM_PER_SHOT;
-  const coffee = makeCoffee(2);
-  console.log(coffee);
+  const maker = new CoffeeMachine(32); // instance level
+  const maker2 = CoffeeMachine.makeMachine(1); // class level
+  console.log(maker);
+  console.log(maker.makeCoffee(2));
+  console.log(maker2);
 }
