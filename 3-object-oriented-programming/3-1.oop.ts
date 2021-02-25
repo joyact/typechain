80d12986fea446b20765a041579807b18d55df6b
Joy Lee <joylee.dev@gmail.com>
2021-02-25 12:11:20 +0900

    3 - Object Oriented Programming : Inheritance

--- 3-object-oriented-programming/3-1.oop.ts
+++ 3-object-oriented-programming/3-1.oop.ts
@@ -4,25 +4,14 @@
     hasMilk: boolean;
   };
 
-  // 2. interface를 이용해 추상화
-  // <클래스명> implements <interface> : interface 형식을 반드시 가지는 class
   interface BasicOption {
     makeCoffee(shots: number): CoffeeCup;
   }
 
-  interface SpecialOption {
-    makeCoffee(shots: number): CoffeeCup;
-    fillCoffeeBeans(beans: number): void;
-    clean(): void;
-  }
+  class CoffeeMachine implements BasicOption {
+    private static BEANS_GRAM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
 
-  // public (default) | private : 외부접근 x | protected : 외부접근 x 상속받는 자식 class는 접근 가능
-  class CoffeeMachine implements BasicOption, SpecialOption {
-    // member variables ( private : 외부에서 접근 못하게 )
-    private static BEANS_GRAM_PER_SHOT: number = 7; // class level : <클래스명.변수명>
-    private coffeeBeans: number = 0; // instance(object) level : <this.변수명>
-
-    // 인스턴스 생성 시 항상 호출되는 함수
     constructor(coffeeBeans) {
       this.coffeeBeans = coffeeBeans;
     }
@@ -31,7 +20,6 @@
       return new CoffeeMachine(coffeeBeans);
     }
 
-    // 1. encapsulation을 사용해 추상화. 사용자가 접근 가능한 함수를 정의
     private grindBeans(shots) {
       let requiredBeans = shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
       if (this.coffeeBeans < requiredBeans) {
@@ -56,7 +44,6 @@
       };
     }
 
-    // 외부에서 멤버변수에 바로 접근하지않고 method를 통해 접근
     fillCoffeeBeans(beans: number) {
       if (beans < 0) {
         throw new Error('value for beans should  be greater than 0');
@@ -77,45 +64,30 @@
     }
   }
 
-  /*
-  * instance level
-    const maker = new CoffeeMachine(32);
-  * class level
-    각 interface 별로 약속된 method만 사용한다
-    const maker1: BasicOption = CoffeeMachine.makeMachine(100);
-    maker1.makeCoffee(5);
+  // 클래스 '상속' 받아 새 클래스 만들기!
+  // <자식클래스명> extends <부모클래스명>
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, serialNumber: string) {
+      super(beans); // 자식 클래스에서 constructor 만들땐 반드시 부모 클래스의 데이터도 함께 적용
+    }
 
-    const maker2: SpecialOption = CoffeeMachine.makeMachine(100);
-    maker2.makeCoffee(1);
-    maker2.fillCoffeeBeans(10);
-    maker2.clean();
-  */
+    private steamMilk() {
+      console.log('>> Steaming some milk');
+    }
 
-  class AmateurUser {
-    constructor(public machine: BasicOption) {}
-    makeCoffee() {
-      console.log('☕ An amateur is making coffee ☕');
-      console.log(this.machine.makeCoffee(5));
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots); // 부모 클래스에서 함수 호출
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
     }
   }
 
-  class ProBarista {
-    constructor(public machine: SpecialOption) {}
-    makeCoffee() {
-      console.log('☕ A barista is making coffee ☕');
-      console.log(this.machine.makeCoffee(3));
-      this.machine.fillCoffeeBeans(50);
-      this.machine.clean();
-    }
-  }
+  const coffee = new CoffeeMachine(50);
+  const latte = new CaffeLatteMachine(50, 'ABC');
 
-  const maker: CoffeeMachine = CoffeeMachine.makeMachine(100);
-
-  // 동일한 CoffeeMachine 클래스를 사용하지만 각자 다른 interface를 가져오기때문에
-  // interface에 따라 클래스내부 함수를 다르게 사용할 수 있다.
-  const amateur = new AmateurUser(maker);
-  const pro = new ProBarista(maker);
-
-  amateur.makeCoffee();
-  pro.makeCoffee();
+  coffee.makeCoffee(1);
+  latte.makeCoffee(2);
 }
