b49b55e0c99744cc3f70e881a07479332aab37a1
Joy Lee <joylee.dev@gmail.com>
2021-02-25 15:29:31 +0900

    3 - Object Oriented Programming : Composition with interface

--- 3-object-oriented-programming/3-1.oop.ts
+++ 3-object-oriented-programming/3-1.oop.ts
@@ -65,8 +65,17 @@
     }
   }
 
+  // de-coupling with interface
+  interface iMilkFrother {
+    makeMilk(cup: CoffeeCup): CoffeeCup;
+  }
+
+  interface iSugarProvider {
+    addSugar(cup: CoffeeCup): CoffeeCup;
+  }
+
   // 우유 제조기
-  class CheapMilkSteamer {
+  class CheapMilkSteamer implements iMilkFrother {
     private steamMilk(): void {
       console.log('>> Steaming some milk');
     }
@@ -79,10 +88,52 @@
     }
   }
 
+  // 고급 우유 제조기
+  class FancyMilkSteamer implements iMilkFrother {
+    private steamMilk(): void {
+      console.log('>> Fancy Steaming some milk');
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
+  // 찬 우유 제조기
+  class ColdMilkSteamer implements iMilkFrother {
+    private steamMilk(): void {
+      console.log('>> Steaming some cold milk');
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
   // 설탕 제조기
-  class CandySugarMixer {
+  class CandySugarMixer implements iSugarProvider {
     private getSugar(): void {
       console.log('>> Getting some sugar');
+    }
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      this.getSugar();
+      return {
+        ...cup,
+        hasSugar: true,
+      };
+    }
+  }
+
+  // 설탕 제조기
+  class BlackSugarMixer implements iSugarProvider {
+    private getSugar(): void {
+      console.log('>> Getting some black sugar');
     }
     addSugar(cup: CoffeeCup): CoffeeCup {
       this.getSugar();
@@ -98,7 +149,7 @@
     constructor(
       beans: number,
       public serialNumber: string,
-      private milk: CheapMilkSteamer
+      private milk: iMilkFrother
     ) {
       super(beans); // 자식 클래스에서 constructor 만들땐 반드시 부모 클래스의 데이터도 함께 적용
     }
@@ -110,7 +161,7 @@
 
   // 설탕 추가
   class SweetCoffeeMachine extends CoffeeMachine {
-    constructor(beans: number, private sugar: CandySugarMixer) {
+    constructor(beans: number, private sugar: iSugarProvider) {
       super(beans);
     }
     makeCoffee(shots: number): CoffeeCup {
@@ -123,8 +174,8 @@
   class SweetCaffeLatteMachine extends CoffeeMachine {
     constructor(
       beans: number,
-      private milk: CheapMilkSteamer,
-      private sugar: CandySugarMixer
+      private milk: iMilkFrother,
+      private sugar: iSugarProvider
     ) {
       super(beans);
     }
@@ -137,8 +188,17 @@
   }
 
   // 기능 별로 클래스를 만들어두고 필요한 곳에서 composition한다
+  // de-coupling : 클래스로 엮인 것들을 interface를 사용해 풀어준다
+  // 클래스 간에 소통이 발생하는 경우, 직접 하는게 아닌 계약서(interface)를 통해 소통한다
+
+  // milk 종류
   const cheapMilkMaker = new CheapMilkSteamer();
+  const fancyMilkMaker = new FancyMilkSteamer();
+  const coldMilkMaker = new ColdMilkSteamer();
+
+  // sugar 종류
   const candySugar = new CandySugarMixer();
+  const blackSugar = new BlackSugarMixer();
 
   const machines = [
     {
@@ -150,12 +210,20 @@
       work: new CaffeLatteMachine(50, 'ABC', cheapMilkMaker),
     },
     {
+      name: 'cold latte',
+      work: new CaffeLatteMachine(50, 'ABC', coldMilkMaker),
+    },
+    {
+      name: 'sweet black Coffee',
+      work: new SweetCoffeeMachine(50, blackSugar),
+    },
+    {
       name: 'sweetCoffee',
       work: new SweetCoffeeMachine(50, candySugar),
     },
     {
       name: 'sweetLatte',
-      work: new SweetCaffeLatteMachine(50, cheapMilkMaker, candySugar),
+      work: new SweetCaffeLatteMachine(50, fancyMilkMaker, candySugar),
     },
   ];
 
