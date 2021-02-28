0c13fb651b6b497389117e6f6ff9bc06d1dd1ff9
Joy Lee <joylee.dev@gmail.com>
2021-02-28 13:23:42 +0900

    5 - Error Handling with type states

--- 5-error/5-2.handling.ts
+++ 5-error/5-2.handling.ts
@@ -1,7 +1,19 @@
 {
+  type NetworkErrorState = {
+    result: 'fail';
+    reason: 'offline' | 'down' | 'timeout';
+  };
+
+  type SuccessState = {
+    result: 'success';
+  };
+
+  type ResultState = SuccessState | NetworkErrorState;
+
   class NetworkClient {
-    tryConnect(): void {
-      throw new Error('no network!');
+    // 예상 가능한 에러는 타입 state로 만들어 정의
+    tryConnect(): ResultState {
+      return;
     }
   }
 
@@ -19,6 +31,7 @@
 
     run() {
       // 에러 발생 시 에러처리를 좀 더 효율적으로 다룰 수 있는 곳에서 try&catch 한다
+      // TypeScript에서 구현된 catch()에는 어떠한 타입정보도 전달되지 않아서 instanceOf를 사용할 수 없다
       try {
         this.userService.login();
       } catch (error) {
